f4325ac5524dcd34219919f9570b0638408fd3e2
stubersx <flows-snips-0u@icloud.com>
2024-11-11 23:10:25 -0500

    Add project files.

---
+++ ViewModels/ArtistViewModel.cs
@@ -0,0 +1,10 @@
+﻿using MusicStore.Models;
+
+namespace MusicStore.ViewModels
+{
+    public class ArtistViewModel
+    {
+        public List<Album> Album { get; set; } = new List<Album>();
+        public List<Artist> Artist { get; set; } = new List<Artist>();
+    }
+}
